67de5949ba98ae62472281d094d8d4a968474c58
hanmant4615 <hanmantchavan2403@gmail.com>
2023-01-22 21:17:19 +0530

    correct url

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json( ))
 app.use(cors({
-origin : ["http://localhost:3000","http://evently.vercel.app"],
-//extra changes
+origin : ["http://localhost:3000","https://evently-app-frontend.vercel.app"],
+//extra chan
 credentials : true,
 }));
 
